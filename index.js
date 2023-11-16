76d7db028eb254b56e1615899d14b339015b37bd
paige <88659700+paishee@users.noreply.github.com>
2023-11-15 21:59:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,18 @@ class FunctionData {
 
         // getting the argument indexes
         let [s, e] = [ strf.indexOf("(")+1, strf.indexOf(")") ];
-        var stra = strf.slice(s, e).bunch();
+        var stra = strf.slice(s, e)
 
+
+        // if arguments exist
+        let argsExist = stra.length > 0 || (eval(`"${stra.content}"`).length > 0 && stra.content)
+
+
+        // if arguments exist bunch them up
+        if (argsExist) stra = stra.bunch();
+
+
+        // creates a shallow copy of the arguments for use in the body
         let argsCopy = stra.toString();
 
 
@@ -47,110 +57,129 @@ class FunctionData {
 
 
         // loop
-        if (stra.length > 0 || (eval(`"${stra.content}"`).length > 0 && stra.content) ) {
+        if (argsExist) {
             while (true) {
+
+                // first try catch to see if it can be turned into an object
+                // for some reason Function and eval give different outputs so this one uses Function
+                
                 try {
+
                     fnr = (new Function(fnv.toString()))(); // fnr = function result
-                    break;
+                    break; // once it has finished it breaks the loop
+
+
+                // catches errors
                 } catch(e) {
 
-                    var ln, pt;
+                    var pt; // the part of the code the error is in
                     var uie = util.inspect(e); // uie = util inspect error
                     
-                    if (e.name == "ReferenceError") {
-                        uie = uie.slice(
+                    if (e.name == "ReferenceError") { // if it's a reference error
+
+                        uie = uie.slice( // slice it to get the first stuff of it
                             uie.indexOf(`at eval (eval at ${this.constructor.name}`), // start
                             uie.indexOf(`at new ${this.constructor.name}`) // end
                         );
 
-                        let slcr = "<anonymous>:";
+                        let slcr = "<anonymous>:"; // thing to slice by
 
-                        uie = uie.slice(
+                        uie = uie.slice( // second slice to get the line and part of the code
                             uie.indexOf(slcr)+slcr.length, // start
                             uie.lastIndexOf(")") // end
                         );
 
-                        [ln, pt] = ((spl=uie.split(":")) => [spl[0], spl[1]] )();
+                        pt = uie.split(":")[1]; // gets the part
 
-                        fnv.append(pt, ":null");
+                        fnv.append(pt, ":null"); // defines anything with no default value as null
                     }
 
-                    else if (e.name == "SyntaxError") {
+
+                    else if (e.name == "SyntaxError") { // if it's a syntax error
+                        
+                        // second try catch which uses eval instead of Function for specific details
+                        
                         try {
-                            eval(fnv.toString().replace("return ", ""))
+                            
+                            eval(fnv.toString().replace("return ", "")); // removes the return statement for the eval
+                        
+
+                        // catches any new errors from the eval
                         } catch(ets) { // ets = e the second
 
+                            // checks if the cause is because something isn't defined
                             if (ets.message.endsWith("is not defined")) {
-                                var uiets = util.inspect(ets);
 
-                                uiets = uiets.slice(
+                                var uiets = util.inspect(ets); // uiets = util inspect e the second
+
+                                uiets = uiets.slice( // slice again to get the stuff
                                     uiets.indexOf(`at eval (eval at ${this.constructor.name}`), // start
                                     uiets.indexOf(`at new ${this.constructor.name}`) // end
                                 );
             
-                                let slcr = "<anonymous>:";
+                                let slcr = "<anonymous>:"; // thing to slice by
             
-                                uiets = uiets.slice(
+                                uiets = uiets.slice( // second slice to get the line and part of the code
                                     uiets.indexOf(slcr)+slcr.length, // start
                                     uiets.lastIndexOf(")") // end
                                 );
             
-                                [ln, pt] = ((spl=uiets.split(":")) => [spl[0], spl[1]] )();
+                                pt = uiets.split(":")[1]; // gets the part
 
-                                stra.append(pt-1, ":null");
+                                stra.append(pt-1, ":null"); // defines anything with no default value as null
                             }
 
+
+                            // if it's an unexpected token telling it that there's an issue with the thing defined
                             else if (ets.message == "Unexpected token ':'") {
 
-                                stra.set(pt-1, "");
+                                stra.set(pt-1, ""); // remove equal signs
 
+                                // redefine the arguments string
                                 stra = ((str=stra.toString()) => new Noodle(
                                     `${
-                                        str.substr(0, pt-2)
+                                        str.substr(0, pt-2) // sub out the beginning
                                     }:${
+                                        // remove the "null" but leave the rest at the end
                                         ((sub=str.substr(pt-1, str.length)) => sub.slice(4, sub.length))()
                                     }`
                                 ))();
                             }
 
-                            else throw ets;
+                            else throw ets; // if it's neither throw the error as is
 
-                            fnv = Noodle.from(`return {${stra}}`);
+                            fnv = Noodle.from(`return {${stra}}`); // redefine fnv
                         }
                     }
 
-                    else throw e;
+                    else throw e; // if it's neither throw the error as is
                 }
             }
         }
 
 
         this.name = name; // name of the function
-        this.dataName = f.name;
+        this.dataName = f.name; // name of the original function
 
 
         // body of the function
         this.body = strf.toString()
-            .replace("function", "").trim()
-            .replace(`${name}`, "").trim()
-            .replace(`(${ (argsCopy.toString()) ? argsCopy.toString() : "" })`, "").trim()
+            .replace("function", "").trim() // remove the function text
+            .replace(`${name}`, "").trim() // remove the name of the function
+            .replace(`(${ (argsCopy.toString()) ? argsCopy.toString() : "" })`, "").trim() // remove the arguments
         
-        this.body = this.body.slice(1, this.body.length-1).trim();
+        this.body = this.body.slice(1, this.body.length-1).trim(); // remove the brackets
 
 
         // function arguments
         this.arguments = Soup.from(fnr)
-            .map( (k, v, i) => (typeof v == "string") ? v.trim() : v)
-            .mapKeys( (k, v, i) => (typeof k == "string") ? k.trim() : k )
-            .pour();
+            .map( (k, v, i) => (typeof v == "string") ? v.trim() : v) // remove white space in values
+            .mapKeys( (k, v, i) => (typeof k == "string") ? k.trim() : k ) // remove white space in keys
+            .pour(); // pour down into an object
 
 
         // function data
-        try {
-            (new Function('t', `t.data = ${strf.toString()}`))(this);
-        } catch {
-            this.data = f;
-        }
+        this.data = f;
 
 
         // a string version of the function
